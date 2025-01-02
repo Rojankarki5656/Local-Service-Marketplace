b15d824e381f8feedd1172736398afcc40788f2d
Rojankarki5656 <www.rojan2020@gmail.com>
2025-01-02 20:25:14 +0545

    changes

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,150 +1,134 @@
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:taxi_booking_flutter/pages/customer.dart';
+import 'package:taxi_booking_flutter/pages/serviceprovider.dart';
+import 'package:taxi_booking_flutter/pages/signup.dart';
+
 
 class Homepage extends StatelessWidget {
   Homepage({super.key});
 
   final TextEditingController username = TextEditingController();
   final TextEditingController password = TextEditingController();
-  Future<void> check_login(context) async
-  {
-    if (username.text != '' || password.text != '')
-    {
-      try
-      {
-        var uri = 'http://10.0.2.2/practise/sql.php';
-        var res = await http.post(Uri.parse(uri), body:{
-          'username': username.text,
-          'password': password.text
-        });
-        print(res.body);
-        var response = jsonDecode(res.body);
-        if (response["success"]==true)
-        {
-          Navigator.push(context, MaterialPageRoute(builder: 
-          (context) => const Customer(),));
-        }
-        else
-        {
-          print('Invalid  username or password');
-        }
-      }
-      catch(e)
-      {
-        print(e);
-      }
-    }
-    else
-    {
-      print('All is not well');
-    }
-  }
-
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:appBar(),
       body: body(context),
     );
   }
 
-  Container body(context) {
-    return Container(
-      height: 400,
-      width: 500,
-      margin:const EdgeInsets.only(top:110,left:150,right:20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: const Color(0xffDDDADA),
-        boxShadow: [
-            BoxShadow(
-              color: const Color.fromARGB(255, 31, 29, 29).withOpacity(0.11),
-              blurRadius: 40,
-              spreadRadius: 0.0
-            )
-          ]
-      ),
-
-      child: Column(
-        children: [
-          SizedBox(
-            width: 300,
-            child: Column(
-              children: [
-              const SizedBox(height: 15,),
-              TextField(
-                controller: username,
-                decoration: const InputDecoration(
-                  hintText: 'Username',
-                  contentPadding: EdgeInsets.all(15),
-                  hintStyle: TextStyle(
-                    fontSize: 16,
-                    color: Color.fromARGB(255, 144, 140, 140)
-                  )
-            
-                ),
-              ),
-              TextField(
-                controller: password,
-                  decoration: const InputDecoration(
-                  hintText: 'Pasword',
-                  contentPadding: EdgeInsets.all(15),
-                  hintStyle: TextStyle(
-                    fontSize: 16,
-                    color: Color.fromARGB(255, 144, 140, 140)
-                  )
-                ),
-              ),
-              const SizedBox(height: 10,),
-              ElevatedButton(onPressed: (){},
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.all(5),
-              ),
-              child: const Text(
-                'Forgot Password?',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 11
+body(context) {
+    return Column(
+      children: [
+            SafeArea(
+              child: Center(
+                child: Text('Local Service \n Marketplace', style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+                fontFamily: 'Poppins' ,
+                shadows: [
+                  Shadow(
+                    offset:const  Offset(4.0, 2.0),
+                    blurRadius: 3.0,
+                    color:const  Color.fromARGB(255, 31, 29, 29).withOpacity(0.15),
                   ),
-                ),),
-              const SizedBox(height: 30,),
-              ElevatedButton(onPressed: () {
-                check_login(context);
-              }, child: const Text('Login')
+                ]
+               ),
+               ),
+      ),
+            ),
+        
+        Expanded(
+          child: Center(
+            child: Container(
+              height: 350,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Theme.of(context).colorScheme.primaryContainer,
+                boxShadow: [
+                    BoxShadow(
+                      color:const  Color.fromARGB(255, 31, 29, 29).withOpacity(0.30),
+                      blurRadius: 40,
+                      spreadRadius: 0.0
+                    )
+                  ]
               ),
-              const SizedBox(height: 20,),
-              ElevatedButton(onPressed: (){
-                
-              }, child: const Text('Create New Account')
-              ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextField(
+                        controller: username,
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          filled: true,
+                          fillColor:const  Color.fromARGB(31, 31, 31, 31),
+                          contentPadding:const  EdgeInsets.all(20),
+                          hintStyle:const TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 144, 140, 140)
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none
+                          )
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:const  EdgeInsets.only(left: 20.0, right: 20.0,bottom: 20),
+                      child: TextField(
+                        controller: password,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          filled: true,
+                          fillColor:const  Color.fromARGB(31, 31, 31, 31),
+                          contentPadding:const  EdgeInsets.all(20),
+                          hintStyle:const  TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 144, 140, 140)
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none
+                          )
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Dashboard(),));
+                      }, child: const Text('Login',style: TextStyle(
+                                  color: Color(0xffC58BF2),
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 14
+                                ),
+                                ),
+                                ),
+                    ),
+                    ElevatedButton(onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const Signup(),));
                       
-            ],
-            )
+                    }, child: const Text('Signup',style: TextStyle(
+                                color: Color(0xffC58BF2),
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14
+                              ),
+                              ),
+                              )
+                  ],
+                ),
+              
+              
+            ),
           ),
-        ],
-      )
-      
-      
-    );
-  }
-
-
-  AppBar appBar() {
-    return AppBar(
-      title: const Text('Taxi Booking',
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 18,
-        fontWeight: FontWeight.bold
-      ),),
-      backgroundColor: Colors.white,
-      elevation: 0.0,
-      centerTitle: true,
-      
+        ),
+      ],
     );
   }
 }
